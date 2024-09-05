dff34d9e1504625e6154d19bd3648bd25579268f
Simon <work.sergey@list.ru>
2024-09-05 23:58:58 +0300

    create test

---
+++ src/test/java/org/example/lesson_4_7/MainPageTest.java
@@ -0,0 +1,63 @@
+package org.example.lesson_4_7;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    public void clickLinkNumber(List<WebElement> list, int number){
+        list.get(number).click();
+    }
+
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void clickFirstSearchedLink() {
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys("Selenium");
+
+        WebElement searchButton = driver.findElement(By.cssSelector("[for = sb_form_go]"));
+        searchButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".b_attribution > cite")));
+        List<WebElement> listOfLinks = driver.findElements(By.cssSelector(".b_attribution > cite"));
+
+        assertEquals("https://www.selenium.dev/", listOfLinks.get(0));
+
+        clickLinkNumber(listOfLinks, 0);
+
+        ArrayList tabs = new ArrayList<> (driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1).toString());
+    }
+
+}
