f8045907fb51d7503a1da248a3b789e8be0b0c03
Simon <work.sergey@list.ru>
2024-09-07 15:53:22 +0300

    refactor test

---
+++ src/test/java/org/example/lesson_5_2/pages/MainPage.java
@@ -0,0 +1,25 @@
+package org.example.lesson_5_2.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+//https://www.bing.com/
+
+public class MainPage {
+
+    @FindBy(css = "#sb_form_q")
+    private WebElement searchField;
+
+    public void search(String searchQuery){
+        searchField.sendKeys(searchQuery);
+        System.out.println("Введён следующий поисковый запрос: " + searchQuery);
+        searchField.submit();
+    }
+
+    public MainPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+}
